3b0e7e0f9e5daee5aa6cefac422d3fd85570fd1f
Minjire <minjirekelvin@gmail.com>
2021-01-08 10:06:11 +0300

    Dataframes combined

--- data_processing.py
+++ data_processing.py
@@ -1,15 +1,53 @@
-#%% imports
+# %% imports
 import pandas as pd
 
-#%% read data
+# %% read data
 df1 = pd.read_csv('stock_sentiment.csv')
 df2 = pd.read_csv('tweet_sentiment.csv')
 
 print(df1.head())
+# print horizontal asterisks
+print(''.ljust(70, '*'))
 print(df2.head())
+print(''.rjust(70, '*'))
 
+print("\nDataframes Shapes:")
 print(df1.shape)
 print(df2.shape)
 
+print("\nDataframes information:")
 print(df1.describe())
+print(''.ljust(70, '*'))
 print(df2.describe())
+
+#%% combine dataframes
+# assign same column names
+df2.columns = df1.columns
+
+print(df1.Sentiment.unique())
+print(df2.Sentiment.unique())
+
+# check data types
+print('\n', df1.dtypes)
+print(''.ljust(25, '*'))
+print(df2.dtypes)
+# change 0(negative) to -1 in df1 to align with df2
+df1.Sentiment.replace(0, -1, inplace=True)
+print(df1.Sentiment.unique())
+
+# append dataframes
+stock_df = df1.append(df2, ignore_index=True)
+
+print(f"Dataframe Shape: {stock_df.shape}")
+print(f"Sentiment unique values: {stock_df.Sentiment.unique()}")
+print(stock_df.tail(10))
+
+# %%
+# check for null values
+print(stock_df.isnull().sum())
+# display null rows
+print(stock_df[stock_df.isna().any(axis=1)])
+# drop null rows and reset index
+stock_df.dropna(inplace=True)
+stock_df.reset_index(drop=True, inplace=True)
+print(stock_df.tail(10))
