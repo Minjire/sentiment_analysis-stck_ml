98f974745a21f7d54341e372fa448afd69ec5754
Minjire <minjirekelvin@gmail.com>
2021-02-04 09:36:37 +0300

    Predict Collected News

--- src/sentiment/predict.py
+++ src/sentiment/predict.py
@@ -3,6 +3,7 @@ import pandas as pd
 # from src.sentiment.data_processing import preprocess, remove_punc
 from src.sentiment.remove_punctuation import remove_punc
 from src.sentiment.preprocess import preprocess
+from tensorflow import keras
 
 # %% read data for specific day(today)
 safaricom_df = pd.read_csv('data/raw/scraper/safaricom.csv')
@@ -21,6 +22,8 @@ safaricom_df['Text Without Punc & Stopwords Joined'] = safaricom_df['Text Withou
     lambda x: " ".join(x))
 print(safaricom_df.head(10))
 
+# %% load model
+model = keras.models.load_model('final_model/final_model.h5')
 
 # %% reset pandas column display
 pd.options.display.max_columns = 0
